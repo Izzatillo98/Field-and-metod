de5f302b0cbe941e0839d4a8c8f4180772858a46
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-06-26 17:03:41 +0500

    Add project files.

---
+++ Field and metod/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace Field_and_metod.Models
+{
+    internal class Contact
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        
+    }
+}
---
+++ Field and metod/Servises/ContactServise.cs
@@ -0,0 +1,37 @@
+﻿using Field_and_metod.Models;
+
+namespace Field_and_metod.Servises
+{
+    internal class ContactServise
+    {
+        private Contact[] contacts = new Contact[10];
+        
+        public void PopulateContacts()
+
+        {
+            contacts[0] = new Contact
+            {
+                Phone = "933718998",
+                Name = "Tillo"
+            };
+
+            contacts[1] = new Contact
+            {
+                Phone = "972140207",
+                Name = "Dadam"
+            };
+        }
+
+        public void ShowAllDate()
+        {
+            foreach (Contact contact in contacts ) 
+            {
+                Console.WriteLine(contact.Name);
+                Console.WriteLine(contact.Phone);
+            }
+            
+        }
+       
+    }
+    
+}
